e258e0a204db96bb4845b3ca52402a3dc607c4d1
antiapplewin <applewin2011@gmail.com>
2025-11-14 23:37:49 +0900

    Update generate_spells.rb

--- _plugins/generate_spells.rb
+++ _plugins/generate_spells.rb
@@ -6,21 +6,27 @@ module Jekyll
 
     def generate(site)
       spells = site.data["spell_save"]
+      
+      return unless spells && spells.is_a?(Array)
 
       spells.each do |spell|
+        next unless spell && spell["name"]
         site.pages << SpellPage.new(site, spell)
       end
     end
   end
 
-  class SpellPage < Page
+  class SpellPage < Jekyll::PageWithoutAFile
     def initialize(site, spell)
       @site = site
       @base = site.source
-      @dir  = "spells/#{spell['name']}"
+      
+      # URL 안전한 경로 생성
+      spell_name = spell["name"] || "unknown"
+      @dir  = "spells/#{spell_name}"
       @name = "index.html"
 
-      self.process(@name)
+      super(site, @base, @dir, @name)
       
       # 레이아웃 설정
       self.data["layout"] = "spell"
